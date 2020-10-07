463df5fbf700399ef9cf899388c40e34c576b488
LautaroKc <lautaroleiva94@gmail.com>
2020-10-07 12:30:26 -0300

    implemento js en header

---
+++ sitio/public/javascripts/headerNav.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(){
+    let navItems = document.querySelectorAll('#headerNav a');
+    for (let navItem of navItems){
+        navItem.addEventListener('mouseover', function(){
+            navItem.style.transition = "1s";
+        })
+    }
+    let carritoIcono = document.querySelector('#cajacarrito i');
+    carritoIcono.addEventListener('mouseover', function(){
+        carritoIcono.style.transition = "1s";
+        carritoIcono.style.transform = "scale(1.2)";
+        carritoIcono.addEventListener('mouseout', function(){
+            carritoIcono.style.transform = "none";
+        })
+    })
+})
